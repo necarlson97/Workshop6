56a69e6c7f9dc56c80af121925a7488f4dc6ce6a
Nils Carlson <nilscarlson1997@gmail.com>
2017-04-13 17:17:26 -0400

    static

--- server/src/server.js
+++ server/src/server.js
@@ -31,3 +31,7 @@ app.post('/reverse', function (req, res) {
     }
 
 });
+
+// You run the server from `server`, so `../client/build` is `server/../client/build`.
+// '..' means "go up one directory", so this translates into `client/build`!
+app.use(express.static('../client/build'));
